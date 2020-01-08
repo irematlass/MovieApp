bdfbfefd0661d49f9907cd306c7a1320ec7bd770
irem <irem.atlas@windowslive.com>
2020-01-08 21:53:30 +0300

    initial commit

---
+++ app/src/main/java/com/example/movieapp/single_movie_details/SingleView.kt
@@ -0,0 +1,13 @@
+package com.example.movieapp.single_movie_details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.movieapp.R
+
+class SingleView : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single_view)
+    }
+}
